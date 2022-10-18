db346cbff08f20e253e5aeb780f0f4a2a2b36543
shalant <doug.rosenberg@gmail.com>
2022-10-18 16:00:54 -0500

    11:56, got bored

---
+++ angtut/src/app/app.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit, OnDestroy } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit, OnDestroy {
+  intervalSub:any = '';
+
+  ngOnInit():void {
+    this.intervalSub = setInterval(() => {
+      console.log('Hello from ngOnInit');
+    }, 1000);
+  }
+
+  ngOnDestroy():void {
+    if(this.intervalSub) {
+      clearInterval(this.intervalSub);
+    }
+  }
+}
